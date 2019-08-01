e8785d7131dc67baa92e321465108b9c3bea27e8
Mark Chen <markchen@Markde-MacBook-Pro.local>
2019-08-01 13:57:34 +0800

    Add environment Vars

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshint esversion:6
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+// connect to database
+mongoose.connect('mongodb://localhost/userdb', { useNewUrlParser: true });
+//set up Schema
+const userSchema = new mongoose.Schema({email: String, password: String});
+
+// var secret = "This is our little secret."
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+const User = mongoose.model('User', userSchema);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/register', function(req, res){
+  res.render('register')
+});
+
+app.post('/register', function(req, res){
+  newUser = new User({email: req.body.username, password: req.body.password});
+  newUser.save(function(err){
+    if (!err){
+      res.render('secrets')
+    }
+  });
+
+})
+
+app.listen(3000, function(){
+  console.log("server started at port 3000.");
+});
